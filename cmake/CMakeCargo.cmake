0b1cf99bc83593762dfb0a5572cd600de5a5e7f8
Alik Aslanyan <cplusplus256@gmail.com>
2018-12-09 21:38:55 +0400

    WORKING?

--- cmake/CMakeCargo.cmake
+++ cmake/CMakeCargo.cmake
@@ -58,10 +58,10 @@ function(cargo_build)
 
     file(GLOB_RECURSE LIB_SOURCES "*.rs")
 
-    if(${CMAME_CARGO_ADDITIONAL_ENV})
+    if(CMAME_CARGO_ADDITIONAL_ENV)
         message("Passed cargo arguments: ${CMAME_CARGO_ADDITIONAL_ENV}")
     elseif()
-    
+
     set(CARGO_ENV_COMMAND ${CMAKE_COMMAND} -E env ${CMAME_CARGO_ADDITIONAL_ENV} PKG_CONFIG_ALLOW_CROSS=1 "CARGO_TARGET_DIR=${CARGO_TARGET_DIR}")
 
     add_custom_command(
