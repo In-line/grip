22b82a3ecd7aa5d9828266fdc2c31c10fcfbf762
Alik Aslanyan <cplusplus256@gmail.com>
2019-07-13 21:32:32 +0400

    Bump version

--- cpp/helper/moduleconfig.h
+++ cpp/helper/moduleconfig.h
@@ -26,7 +26,7 @@
  *   to add multiple entries.
  */
 #define MODULE_NAME "gRIP"
-#define MODULE_VERSION "0.1.4 Beta"
+#define MODULE_VERSION "0.1.5 Beta"
 #define MODULE_AUTHOR "Inline"
 #define MODULE_URL "https://github.com/in-line/grip"
 #define MODULE_LOGTAG "gRIP"
